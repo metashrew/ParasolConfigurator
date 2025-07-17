15145c55c3a6382f2ff043e9a7cd7792dcddac81
jannesHP <jannes.de.koning@telenet.be>
2025-07-17 11:20:59 +0200

    new model. new laptop

--- parasolconfigurator/src/components/Floor.tsx
+++ parasolconfigurator/src/components/Floor.tsx
@@ -5,7 +5,7 @@ type Props = {}
 export default function Floor({}: Props) {
   return (
     <mesh rotation={[-Math.PI/2,0,0]}>
-      <circleGeometry args={[2]}/>
+      <circleGeometry args={[2, 48]}/>
     </mesh>
   )
 }
