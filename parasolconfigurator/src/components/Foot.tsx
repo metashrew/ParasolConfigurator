0e9a7b3cdf12eeaafdb14ef17864dc9ddebdf075
Jannes De Koning <jannes.dekoning@vdabcampus.be>
2025-07-31 15:47:01 +0200

    foot cleanup

--- parasolconfigurator/src/components/Foot.tsx
+++ parasolconfigurator/src/components/Foot.tsx
@@ -1,18 +1,13 @@
 import { useGLTF } from "@react-three/drei/core/Gltf"
-import { useMemo } from "react"
-import { MeshStandardMaterial, Shape, type ExtrudeGeometryOptions } from "three"
-import { Group } from "three/examples/jsm/libs/tween.module.js"
 
 type Props = {
     size: number
-    height?: number
 }
 
-export default function Foot({size, height = 0.075}: Props) {
-  
+export default function Foot({size}: Props) {
+
   const gltf = useGLTF(`./src/assets/foot.glb`)
   const remappedValue = (size - 30) / 30
-  console.log(remappedValue)
   gltf.nodes["Cylinder"].morphTargetInfluences[0] = remappedValue
 
   return (
